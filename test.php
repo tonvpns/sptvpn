be3e4714fbe02ada0ba4564a65a01556f695fd3b
tonvpn5g <58139201+tonvpns@users.noreply.github.com>
2021-11-15 22:25:08 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,7 @@
+echo "# sptvpn" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/tonvpns/sptvpn.git
+git push -u origin main
